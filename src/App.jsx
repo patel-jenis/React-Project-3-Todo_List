e83c8d05964107e4d9ee29720ccb72e3e5d21ef8
patel-jenis <jenismpatel2004@gmail.com>
2026-02-06 11:32:48 +0530

    Add Update, Delete and Filtering Functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React, { useRef, useState } from 'react'
 
 const App = () => {
     const [toDo, setTodo] = useState([]);
+    const [isEdit, setIsEdit] = useState(false);
+    const [editIdx, setEditIdx] = useState(null);
+    const [filter, setFilter] = useState("all");
 
     const inputRef = useRef(null);
     const selectRef = useRef(null);
@@ -11,21 +14,58 @@ const App = () => {
         let taskName = inputRef.current.value;
         let priority = selectRef.current.value;
 
-        let taskObj = {
-            id: Date.now(),
-            taskName, priority
-        }
-
         if (taskName.trim() === "" || priority.trim() === "") {
             alert("Please Fill All Details....");
             return;
         }
 
-        setTodo([...toDo, taskObj]);
+        if (isEdit && editIdx) {
+            const updatedTodo = toDo.map((t) => {
+                if (t.id === editIdx) {
+                    return {
+                        id: t.id,
+                        taskName: taskName,
+                        priority: priority
+                    };
+                }
+                return t;
+            });
 
+            setTodo(updatedTodo);
+            setIsEdit(false);
+            setEditIdx(null);
+        } else {
+            let taskObj = {
+                id: Date.now(),
+                taskName, priority
+            }
+            
+            setTodo([...toDo, taskObj]);
+        }
+        
         inputRef.current.value = "";
         selectRef.current.value = "";
     }
+
+    const handleDeleteTask = (id) => {
+        let newTodo = toDo.filter((task) => task.id !== id);
+        setTodo(newTodo);
+    }
+
+    const handleUpdateTask = (id) => {
+        const task = toDo.find((t) => t.id === id);
+
+        inputRef.current.value = task.taskName;
+        selectRef.current.value = task.priority;
+
+        setIsEdit(true);
+        setEditIdx(id);
+    }
+
+    const filteredTasks =
+        filter === "all"
+            ? toDo
+            : toDo.filter((task) => task.priority === filter);
 
     return (
         <div className="container">
@@ -52,26 +92,57 @@ const App = () => {
                                 </div>
                                 <div className="col-lg-2">
                                     <div>
-                                        <button className='py-3 px-0 btn btn-dark w-100 text-nowrap'>Add Task</button>
+                                        <button className='py-3 px-0 btn btn-dark w-100 text-nowrap'>{isEdit ? "Update" : "Add"}</button>
                                     </div>
                                 </div>
                                 <div className="col-12">
+                                    <div className="mt-4 d-flex justify-content-end">
+                                        <select
+                                            className="form-select w-25"
+                                            onChange={(e) => setFilter(e.target.value)}>
+                                            <option value="all">All Tasks</option>
+                                            <option value="1">High Priority</option>
+                                            <option value="2">Medium Priority</option>
+                                            <option value="3">Low Priority</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div className="col-12 m-0">
                                     {
-                                        toDo.map((task) => {
-                                            return <div className='my-3 clr p-3 rounded-2 d-md-flex align-items-center justify-content-between' key={task.id}>
-                                                <h5 className='m-md-0 mb-3 fs-5'>{task.taskName}</h5>
-                                                <div className='d-flex align-items-center gap-5 justify-content-between'>
-                                                    <p className={`py-2 px-4 m-0 text-light bg-${task.priority === "1" ? "success"
-                                                        : task.priority === "2" ? "warning" : "danger"} bg-gradient rounded-2`}>
-                                                        {task.priority === "1" ? "High"
-                                                            : task.priority === "2" ? "Medium" : "Low"}
-                                                    </p>
-                                                    <div>
-                                                        <button className='btn bg-danger bg-gradient bg-opacity-75 py-2 px-3 m-0'>X</button>
-                                                    </div>
-                                                </div>
+                                        filteredTasks.length === 0 ? (
+                                            <div className="clr p-4 low-black rounded-2 text-center mt-3">
+                                                <h5>
+                                                    No {filter === "all" ? "Any"
+                                                        : filter === "1" ? "High Priority"
+                                                            : filter === "2" ? "Medium Priority"
+                                                                : filter === "3" ? "Low Priority"
+                                                                    : ""} Task Available
+                                                </h5>
                                             </div>
-                                        })
+                                        ) : (
+                                            filteredTasks.map((task) => {
+                                                return (
+                                                    <div className='my-3 clr p-3 rounded-2 d-md-flex align-items-center justify-content-between' key={task.id}>
+                                                        <h5 className='m-md-0 mb-3 fs-5'>{task.taskName}</h5>
+                                                        <div className='d-flex align-items-center gap-sm-5 gap-2 justify-content-between'>
+                                                            <p className={`py-2 px-4 m-0 text-light bg-${task.priority === "1" ? "success"
+                                                                : task.priority === "2" ? "warning" : "danger"} bg-gradient rounded-2`}>
+                                                                {task.priority === "1" ? "High"
+                                                                    : task.priority === "2" ? "Medium" : "Low"}
+                                                            </p>
+                                                            <div className='d-flex align-items-center gap-2'>
+                                                                <button type="button" className='btn bg-danger bg-gradient bg-opacity-75 py-2 px-sm-3 px-2 m-0' onClick={() => handleDeleteTask(task.id)}>
+                                                                    <i className="ri-delete-bin-5-fill" />
+                                                                </button>
+                                                                <button type="button" className='btn bg-warning bg-gradient bg-opacity-75 py-2 px-sm-3 px-2 m-0' onClick={() => handleUpdateTask(task.id)}>
+                                                                    <i className="ri-edit-2-fill" />
+                                                                </button>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                )
+                                            })
+                                        )
                                     }
                                 </div>
                             </div>
